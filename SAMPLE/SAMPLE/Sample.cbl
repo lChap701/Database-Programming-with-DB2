198d84b1ffc09fdfcc6bd920cdb4f0861574f1c4
lChap701 <lucas_chapman@stu.indianhills.edu>
2021-01-28 13:57:55 -0600

    Fixes

---
+++ SAMPLE/SAMPLE/Sample.cbl
@@ -0,0 +1,34 @@
+<<<<<<< HEAD
+       identification division.
+       program-id. Sample.
+
+       environment division.
+       configuration section.
+
+       data division.
+       working-storage section.
+       01 WK-HOLD         PIC X.
+
+       procedure division.
+           Display "HELLO ALL".
+           ACCEPT WK-HOLD.
+           goback.
+           
+=======
+       identification division.
+       program-id. Sample.
+
+       environment division.
+       configuration section.
+
+       data division.
+       working-storage section.
+       01 WK-HOLD         PIC X.
+
+       procedure division.
+           Display "HELLO ALL".
+           ACCEPT WK-HOLD.
+           goback.
+           
+>>>>>>> b6b6cfaf02e6a9020282ae5746b77892f5fa7f33
+       end program Sample.
---
+++ SAMPLE/SAMPLE/SimpleAccess.cbl
@@ -0,0 +1,141 @@
+<<<<<<< HEAD
+       WORKING-STORAGE SECTION.
+       01  WKHOLD                  PIC X.
+       
+           EXEC SQL INCLUDE SQLCA END-EXEC. 
+       
+       01 MFSQLMESSAGETEXT  PIC X(250).  
+       
+            EXEC SQL BEGIN DECLARE SECTION END-EXEC.
+      *       EXEC SQL INCLUDE Patron END-EXEC.        
+       01  DCLEMPLOYEE.
+           03 EMPNUM                     PIC  X(6).
+           03 LASTNAME                   PIC  X(15).
+           EXEC SQL END DECLARE SECTION END-EXEC.
+       
+  
+         
+       PROCEDURE DIVISION.
+       RUN-START.
+           DISPLAY 'START'.
+           ACCEPT WKHOLD.
+           EXEC SQL 
+               WHENEVER SQLERROR perform OpenESQL-Error 
+           END-EXEC.
+           PERFORM CONNECT-TO-DATABASE.
+           PERFORM SELECT-DATA.
+           PERFORM SHUT-DOWN.
+                  
+       CONNECT-TO-DATABASE.    
+            EXEC SQL 
+                CONNECT TO SAMPLE 
+            END-EXEC.
+           
+
+       SELECT-DATA.    
+               MOVE '000320' TO  EMPNUM.
+               EXEC SQL 
+                   SELECT lastname
+                   INTO :LASTNAME
+                   FROM  Employee
+                      WHERE EMPNO = :EMPNUM
+                       
+               END-EXEC. 
+               IF SQLCODE = 0 
+                   display 'Employee ' EMPNUM ' ' LASTNAME
+               ELSE
+                   display 'Employee not found'
+                           
+e               END-IF.
+          
+           
+           DISPLAY 'Press enter to continue'.
+           ACCEPT WKHOLD.
+       SHUT-DOWN.    
+               
+          
+           Display 'Program stopping '
+            ACCEPT WKHOLD.
+           Stop 'Press <CR> to terminate'
+           STOP RUN
+           .
+       OpenESQL-Error Section.
+       
+           display "SQL Error = " sqlstate " " sqlcode 
+           display MFSQLMESSAGETEXT 
+           ACCEPT WKHOLD
+           stop run
+           .
+       
+       
+=======
+       WORKING-STORAGE SECTION.
+       01  WKHOLD                  PIC X.
+       
+           EXEC SQL INCLUDE SQLCA END-EXEC. 
+       
+       01 MFSQLMESSAGETEXT  PIC X(250).  
+       
+            EXEC SQL BEGIN DECLARE SECTION END-EXEC.
+      *       EXEC SQL INCLUDE Patron END-EXEC.        
+       01  DCLEMPLOYEE.
+           03 EMPNUM                     PIC  X(6).
+           03 LASTNAME                   PIC  X(15).
+           EXEC SQL END DECLARE SECTION END-EXEC.
+       
+  
+         
+       PROCEDURE DIVISION.
+       RUN-START.
+           DISPLAY 'START'.
+           ACCEPT WKHOLD.
+           EXEC SQL 
+               WHENEVER SQLERROR perform OpenESQL-Error 
+           END-EXEC.
+           PERFORM CONNECT-TO-DATABASE.
+           PERFORM SELECT-DATA.
+           PERFORM SHUT-DOWN.
+                  
+       CONNECT-TO-DATABASE.    
+            EXEC SQL 
+                CONNECT TO SAMPLE 
+            END-EXEC.
+           
+
+       SELECT-DATA.    
+               MOVE '000320' TO  EMPNUM.
+               EXEC SQL 
+                   SELECT lastname
+                   INTO :LASTNAME
+                   FROM  Employee
+                      WHERE EMPNO = :EMPNUM
+                       
+               END-EXEC. 
+               IF SQLCODE = 0 
+                   display 'Employee ' EMPNUM ' ' LASTNAME
+               ELSE
+                   display 'Employee not found'
+                           
+e               END-IF.
+          
+           
+           DISPLAY 'Press enter to continue'.
+           ACCEPT WKHOLD.
+       SHUT-DOWN.    
+               
+          
+           Display 'Program stopping '
+            ACCEPT WKHOLD.
+           Stop 'Press <CR> to terminate'
+           STOP RUN
+           .
+       OpenESQL-Error Section.
+       
+           display "SQL Error = " sqlstate " " sqlcode 
+           display MFSQLMESSAGETEXT 
+           ACCEPT WKHOLD
+           stop run
+           .
+       
+       
+>>>>>>> b6b6cfaf02e6a9020282ae5746b77892f5fa7f33
